32a9f3a7a1ca08706da0c1e4967a871595f0a62c
Teodor Stefu <Teodorstefu@gmail.com>
2017-02-09 02:21:46 +0200

    Ckan parser

---
+++ parser/ckan.py
@@ -0,0 +1,10 @@
+from ckan_crawl.ckan_crawl import CrawlCKAN
+from config_utils.config import ConfigCrawlYaml
+
+#get config
+configInst=ConfigCrawlYaml()
+config=configInst.get_config()
+
+#do work
+cc=CrawlCKAN(config)
+cc.do_process()
